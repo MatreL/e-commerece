5af77c4f046488eb72fc1f422d6241edd8253c08
MatreL <Evenmatre@gmail.com>
2021-09-08 00:33:21 +0200

    added product API and made cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,78 @@
-import React from 'react'
-import Products from "./components/products/Products";
+import React, { useState, useEffect } from 'react'
+import { commerce } from './lib/commerce'
+
+import { Products, Navbar, Cart, Checkout } from "./components"
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve())
+  };
+
+  const handleAddToCart = async (productId, quanitity) => {
+    const item = await commerce.cart.add(productId, quanitity);
+
+    setCart(item.cart);
+  }
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const response = await commerce.cart.update(productId, { quantity });
+
+    setCart(response.cart);
+  };
+
+  const handleRemoveFromCart = async (productId) => {
+    const response = await commerce.cart.remove(productId);
+
+    setCart(response.cart)
+  }
+
+  const handleEmptyCart = async () => {
+    const response = await commerce.cart.empty()
+
+    setCart(response.cart)
+  }
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  console.log(cart);
+
   return (
-    <div>
-       <Products />
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart} />
+          </Route>
+
+          <Route exact path="/cart">
+            <Cart
+              cart={cart}
+              handleEmptyCart={handleEmptyCart}
+              onRemoveFromCart={handleRemoveFromCart}
+              onUpdateCartQty={handleUpdateCartQty}
+            />
+          </Route>
+
+          <Route exact path="/checkout">
+            <Checkout />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
---
+++ src/components/CheckoutForm/AdresseForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdresseForm = () => {
+  return (
+    <div>
+      AdresseForm
+    </div>
+  )
+}
+
+export default AdresseForm
